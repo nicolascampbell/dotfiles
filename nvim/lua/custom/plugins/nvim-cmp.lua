15e82f4cd71c02f8ba9003c9c295e814b92f5b14
nds <desouza@zuhause-plattform.de>
2023-12-12 09:22:06 +0100

    fix(nvim): luasnip as dep

--- nvim/lua/custom/plugins/nvim-cmp.lua
+++ nvim/lua/custom/plugins/nvim-cmp.lua
@@ -3,7 +3,13 @@ return {
     'hrsh7th/nvim-cmp',
     dependencies = {
         -- Snippet Engine & its associated nvim-cmp source
-        'L3MON4D3/LuaSnip',
+        {
+            'L3MON4D3/LuaSnip',
+            dependencies = {
+
+                'rafamadriz/friendly-snippets',
+            }
+        },
         'saadparwaiz1/cmp_luasnip',
 
         -- Adds LSP completion capabilities
