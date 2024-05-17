aed55122e147648276aaf6e646d48b372d187ac3
nicolascampbell <nicodsm7@gmail.com>
2024-05-17 23:20:55 +0200

    refactor

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -1,5 +1,5 @@
 require("rose-pine").setup({
-	variant = "main",   -- auto, main, moon, or dawn
+	variant = "main", -- auto, main, moon, or dawn
 	dark_variant = "main", -- main, moon, or dawn
 	dim_inactive_windows = false,
 	extend_background_behind_borders = true,
@@ -7,7 +7,7 @@ require("rose-pine").setup({
 	enable = {
 		terminal = true,
 		legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
-		migrations = true,  -- Handle deprecated options automatically
+		migrations = true, -- Handle deprecated options automatically
 	},
 
 	styles = {
@@ -73,6 +73,3 @@ require("rose-pine").setup({
 })
 
 vim.cmd("colorscheme rose-pine")
--- vim.cmd("colorscheme rose-pine-main")
--- vim.cmd("colorscheme rose-pine-moon")
--- vim.cmd("colorscheme rose-pine-dawn")
