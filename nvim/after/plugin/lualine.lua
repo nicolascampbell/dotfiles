2696e82290d3489034355950a2870c92ddd69b72
nicolascampbell <nicodsm7@gmail.com>
2024-06-18 16:23:07 +0200

    chore(general): minor color changes

--- nvim/after/plugin/lualine.lua
+++ nvim/after/plugin/lualine.lua
@@ -2,8 +2,8 @@ require("lualine").setup({
 	options = {
 		theme = "rose-pine",
 		icons_enabled = false,
-		component_separators = '|',
-		section_separators = '',
+		component_separators = "|",
+		section_separators = "",
 		disabled_filetypes = {
 			statusline = {},
 			winbar = {},
@@ -15,17 +15,17 @@ require("lualine").setup({
 			statusline = 1000,
 			tabline = 1000,
 			winbar = 1000,
-		}
+		},
 	},
 	sections = {
-		lualine_a = { 'mode' },
-		lualine_b = { 'branch', 'diff', 'diagnostics' },
+		lualine_a = { "mode" },
+		lualine_b = { "branch", "diff", "diagnostics" },
 		lualine_c = {
 			{
-				'filename',
+				"filename",
 				file_status = true, -- Displays file status (readonly status, modified status)
 				newfile_status = false, -- Display new file status (new file means no write after created)
-				path = 4,   -- 0: Just the filename
+				path = 4, -- 0: Just the filename
 				-- 1: Relative path
 				-- 2: Absolute path
 				-- 3: Absolute path, with tilde as the home directory
@@ -34,28 +34,27 @@ require("lualine").setup({
 				shorting_target = 40, -- Shortens path to leave 40 spaces in the window
 				-- for other components. (terrible name, any suggestions?)
 				symbols = {
-					modified = '[+]', -- Text to show when the file is modified.
-					readonly = '[-]', -- Text to show when the file is non-modifiable or readonly.
-					unnamed = '[No Name]', -- Text to show for unnamed buffers.
-					newfile = '[New]', -- Text to show for newly created file before first write
+					modified = "[+]", -- Text to show when the file is modified.
+					readonly = "[-]", -- Text to show when the file is non-modifiable or readonly.
+					unnamed = "[No Name]", -- Text to show for unnamed buffers.
+					newfile = "[New]", -- Text to show for newly created file before first write
 				},
-			}
+			},
 		},
 		lualine_x = {},
-		lualine_y = { 'encoding', 'fileformat', 'filetype' },
-		lualine_z = { 'progress' }
+		lualine_y = { "encoding", "filetype" },
+		lualine_z = { "progress" },
 	},
 	inactive_sections = {
 		lualine_a = {},
 		lualine_b = {},
-		lualine_c = { { 'filename', path = 4 } },
+		lualine_c = { { "filename", path = 4 } },
 		lualine_x = {},
 		lualine_y = {},
-		lualine_z = {}
+		lualine_z = {},
 	},
 	tabline = {},
 	winbar = {},
 	inactive_winbar = {},
-	extensions = {}
-}
-)
+	extensions = {},
+})
