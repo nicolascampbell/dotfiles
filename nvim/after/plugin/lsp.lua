cf307365a3f7affd43b43e1e724a8d1e9840f970
nicolascampbell <nicodsm7@gmail.com>
2024-11-17 11:10:08 +0100

    chore(nvim): removed ruby stuff

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -78,19 +78,6 @@ local servers = {
 	cssls = { filetypes = { "scss", "css" } },
 	gopls = {},
 	pyright = {},
-	ruby_lsp = {},
-	solargraph = {
-		completion = true,
-		autoformat = false,
-		formatting = true,
-		symbols = true,
-		definitions = true,
-		references = true,
-		folding = true,
-		highlights = true,
-		diagnostics = true,
-		rename = true,
-	},
 	lua_ls = {
 		Lua = {
 			completion = {
